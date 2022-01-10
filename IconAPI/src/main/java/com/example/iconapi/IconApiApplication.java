9c6ac26c37393edc5aed7cc05225727239726169
agusgar409 <aggarcia@fi.uba.ar>
2022-01-10 20:01:17 -0300

    nuevo proyecto

---
+++ IconAPI/src/main/java/com/example/iconapi/IconApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.iconapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IconApiApplication.class, args);
+    }
+
+}
---
+++ IconAPI/src/test/java/com/example/iconapi/IconApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iconapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
